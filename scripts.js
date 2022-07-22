afa4a8868fa241747023e65b9a0fb898dd53d94b
Anna <anna.franklyn@gmail.com>
2022-07-22 11:42:12 +0800

    hamburger menu added
    HTML complete, CSS half finished

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+
+
+    $('.hamburger-button').click(function(){
+        $('.mobile-menu').fadeToggle(100);
+        
+        $(this).toggleClass('active');
+        
+      });
+
+
+
+    
+});
